cdd05dbe0ba12a0fd95b9dd01102ba88984227c5
wbrunovieira <wbrunovieira77@gmail.com>
2021-08-29 22:51:25 +0100

    added styled-components, added global styles, fonts and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
 
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <GlobalStyle />
     </div>
   );
 }
