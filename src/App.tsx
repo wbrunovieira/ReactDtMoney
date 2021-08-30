06a89a6c80e0adf8f7967eba84e34315386add3e
wbrunovieira <wbrunovieira77@gmail.com>
2021-08-30 09:09:10 +0100

    created the componet Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import styled from 'styled-components';
+
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <>
+      <Header  />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
